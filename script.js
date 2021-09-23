cc429cca57924684ffa3880e9b00087717248d75
Renato Alvarado <riar96@gmail.com>
2021-09-23 17:40:15 -0500

    Finished the css style, just need to update the script

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let add = (a,b) => a += b;
 let subtract = (a,b) => a-=b;
 let multiply = (a,b) => a*=b;
 let divide = (a,b) => a = (a/b);
-//let operate = function(operator,a,b) {
-//    
-//}
+let operate = function(operator,a,b) {
+    return operator(a,b);
+};
