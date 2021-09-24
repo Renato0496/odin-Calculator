888cb15e6895a1d4e64c26653907f366c1282094
Renato Alvarado <riar96@gmail.com>
2021-09-24 14:17:37 -0500

    Added the event listener for each operator, clear and equal. Will fix the bugs after lunch

--- script.js
+++ script.js
@@ -6,20 +6,43 @@ let current = document.querySelector('#botDisplay');
 let accumulate = document.querySelector('#topDisplay');
 
 // operators
-let add = (a,b) => a += b;
-let subtract = (a,b) => a-=b;
-let multiply = (a,b) => a*=b;
-let divide = (a,b) => a = (a/b);
-let operate = function(operator,a,b) {
+let add = (a, b) => a += b;
+let subtract = (a, b) => a -= b;
+let multiply = (a, b) => a *= b;
+let divide = (a, b) => a = (a / b);
+let operate = function (operator, a, b) {
     return operator(a,b);
 };
 
 //event listeners
-operands.forEach(oper => oper.addEventListener('click',function(){
-    alert(this.textContent)
+operands.forEach(oper => oper.addEventListener('click', function () {
+    let sign = this.textContent;
+    if (!accumulate.textContent&&current.textContent) {
+        accumulate.innerHTML = `${current.textContent} ${sign}`;
+        current.innerHTML = '';
+    } else if (accumulate.textContent && current.textContent){
+        let first = Number(accumulate.textContent.split(' ')[0]);
+        let signToUse = accumulate.textContent.split(' ')[1];
+        let second = Number(current.textContent);
+        if (signToUse === '+'){
+            let theNewerAccu = operate(add,first,second);
+            accumulate.innerHTML = `${theNewerAccu} ${sign}`;
+        } else if (signToUse === '-'){
+            let theNewerAccu = operate(subtract,first,second);
+            accumulate.innerHTML = `${theNewerAccu} ${sign}`;
+        } else if (signToUse === 'x'){
+            let theNewerAccu = operate(multiply,first,second);
+            accumulate.innerHTML = `${theNewerAccu} ${sign}`;
+        } else if (signToUse === '÷'){
+            let theNewerAccu = operate(divide,first,second);
+            accumulate.innerHTML = `${theNewerAccu} ${sign}`;
+        };
+        current.innerHTML = '';
+    }
 }));
-numbers.forEach(oper => oper.addEventListener('click',function(){
-    if (!current.textContent){
+
+numbers.forEach(oper => oper.addEventListener('click', function () {
+    if (!current.textContent) {
         newOne = [];
         newOne.push(this.value);
         current.innerHTML = newOne;
@@ -30,9 +53,29 @@ numbers.forEach(oper => oper.addEventListener('click',function(){
         current.innerHTML = anotherOne;
     }
 }));
-ac.addEventListener('click',function(){
-    alert(this.textContent)
+
+ac.addEventListener('click', function () {
+    current.innerHTML = '';
+    accumulate.innerHTML = '';
 });
-equal.addEventListener('click',function(){
-    alert(this.textContent)
+equal.addEventListener('click', function () {
+    if (current.textContent && accumulate.textContent){
+        let first = Number(accumulate.textContent.split(' ')[0]);
+        let signToUse = accumulate.textContent.split(' ')[1];
+        let second = Number(current.textContent);
+        if (signToUse === '+'){
+            let theNewerAccu = operate(add,first,second);
+            accumulate.innerHTML = `${theNewerAccu}`;
+        } else if (signToUse === '-'){
+            let theNewerAccu = operate(subtract,first,second);
+            accumulate.innerHTML = `${theNewerAccu}`;
+        } else if (signToUse === 'x'){
+            let theNewerAccu = operate(multiply,first,second);
+            accumulate.innerHTML = `${theNewerAccu}`;
+        } else if (signToUse === '÷'){
+            let theNewerAccu = operate(divide,first,second);
+            accumulate.innerHTML = `${theNewerAccu}`;
+        };
+    };
+    current.innerHTML = '';
 });
